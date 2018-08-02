f9ff7de6d7776afb8ca7df4990ebc78d3800765a
Syed Azmain <12syed17@gmail.com>
2018-08-02 20:47:50 +0000

    Done.

---
+++ test.js
@@ -0,0 +1,9 @@
+var cat = "newspaper"
+
+if (cat === "chilling"){
+  console.log("cat is chillin' right now.")
+} else if (cat === "sleeping") {
+  console.log("cat is sleeping right now.")
+} else {
+  console.log("cat is doing something else.")
+}
