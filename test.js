5fdc16516611b7a40b19752cec35a7998feb4384
Syed Azmain <12syed17@gmail.com>
2018-08-02 20:47:16 +0000

    automatically backed up by learn

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-var cat = "chilling"
+var cat = "sleeping"
 
 if (cat === "chilling"){
   console.log("cat is chillin' right now.")
 } else if (cat === "sleeping") {
   console.log("cat is sleeping right now.")
 } else {
-  console.log("cat is watching tv.")
+  console.log("cat is doing something else.")
 }
